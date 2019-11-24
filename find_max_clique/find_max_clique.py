3490811bf816e5065d10af19d4b983064b173b1c
Jay Li <j_li@g.harvard.edu>
2019-11-24 05:17:58 -0500

    add find max clique package

--- find_max_clique/find_max_clique.py
+++ find_max_clique/find_max_clique.py
@@ -3,12 +3,31 @@
 
 import os
 
-output = os.popen("fmc/src/fmc fmc/testgraphs/hamming6-2.clq.mtx -t 1 -p").read()
+def find_max_clique(fmc_path, input_fpath, heuristic=True):
+    """Call fmc and get max clique nodes
 
-nodes = []
-for line in output.splitlines():
-    token = "Maximum clique:"
-    if line.startswith(token):
-        nodes = [int(x) for x in line[len(token):].split()]
-nodes.sort()
-print(nodes)
+    Args:
+        fmc_path: fmc executable file path
+        input_fpath: input mtx file path
+        heuristic: whether use the heursitic version of fmc
+    Returns:
+        nodes: sorted indices of the max clique nodes
+    """
+    command = fmc_path + " " + input_fpath + " -p"
+    algorithm_type = 0
+    if heuristic:
+        algorithm_type = 1
+    command += " -t {}".format(algorithm_type)
+    output = os.popen(command).read()
+
+    nodes = []
+    for line in output.splitlines():
+        token = "Maximum clique:"
+        if line.startswith(token):
+            nodes = [int(x) for x in line[len(token):].split()]
+    nodes.sort()
+    return nodes
+
+if __name__ == "__main__":
+    nodes = find_max_clique("fmc/src/fmc", "fmc/testgraphs/hamming6-2.clq.mtx")
+    print(nodes)
