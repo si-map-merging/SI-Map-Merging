edb55d3736333d5d709aecea1bb71f74ceb9f84c
Jay Li <j_li@g.harvard.edu>
2019-12-03 05:48:41 -0500

    refactor 2D GTSAM graph

--- gtsam_optimize/optimization.py
+++ gtsam_optimize/optimization.py
@@ -1,6 +1,7 @@
 import gtsam
 import numpy as np
 
+
 def vector3(x, y, z):
     """Create 3d double numpy array."""
     return np.array([x, y, z], dtype=np.float)
@@ -18,34 +19,12 @@ class Graph:
     def __init__(self, single_robot_graph):
         """Build GTSAM graph from single robot graph from process_g2o package
         """
-        gtsam_graph = gtsam.NonlinearFactorGraph()
-        # create a short-hand
-        srg = single_robot_graph
+        self.graph = gtsam.NonlinearFactorGraph()
+        self.srg = single_robot_graph
 
-        # Set up first pose as origin
-        PRIOR_NOISE = gtsam.noiseModel_Diagonal.Sigmas(vector3(0.1, 0.1, 0.1))
-        min_node_idx = min(srg.nodes)
-        min_node = srg.nodes[min_node_idx]
-        gtsam_graph.add(gtsam.PriorFactorPose2(min_node.id_,
-                        gtsam.Pose2(*min_node.pose()),
-                        PRIOR_NOISE))
-
-        # Add odometry factors & loop closure factors
-        for edge in srg.odom_edges.values() + srg.loop_closure_edges.values():
-            i, j = edge.i, edge.j
-            assert(edge.has_diagonal_info())
-            noise = gtsam.noiseModel_Diagonal.Sigmas(
-                            vector3(*edge.diagonal_sigmas()))
-            gtsam_graph.add(gtsam.BetweenFactorPose2(
-                             i, j, gtsam.Pose2(*edge.measurement()), noise))
-
-        # Create initial estimates
-        initial_estimates = gtsam.Values()
-        for node in srg.nodes.values():
-            initial_estimates.insert(node.id_, gtsam.Pose2(*node.pose()))
-
-        self.graph = gtsam_graph
-        self.initial = initial_estimates
+        self.set_anchor()
+        self.add_factors()
+        self.create_initial()
 
     def optimize(self):
         """Optimize the graph
@@ -91,15 +70,55 @@ class Graph:
         """
         gtsam.writeG2o(self.graph, self.result, fpath)
 
+
+class Graph2D(Graph):
+    def set_anchor(self):
+        """Set prior on the first node, to make it an anchor
+        """
+        srg = self.srg
+        gtsam_graph = self.graph
+
+        # Set up first pose as origin
+        PRIOR_NOISE = gtsam.noiseModel_Diagonal.Sigmas(vector3(0.1, 0.1, 0.1))
+        min_node_idx = min(srg.nodes)
+        min_node = srg.nodes[min_node_idx]
+        gtsam_graph.add(gtsam.PriorFactorPose2(min_node.id_,
+                        gtsam.Pose2(*min_node.pose()),
+                        PRIOR_NOISE))
+
+    def add_factors(self):
+        """Add odometry factors & loop closure factors
+        """
+        srg = self.srg
+        gtsam_graph = self.graph
+        for edge in list(srg.odom_edges.values()) + list(
+                     srg.loop_closure_edges.values()):
+            i, j = edge.i, edge.j
+            assert(edge.has_diagonal_info())
+            noise = gtsam.noiseModel_Diagonal.Sigmas(
+                            vector3(*edge.diagonal_sigmas()))
+            gtsam_graph.add(gtsam.BetweenFactorPose2(
+                             i, j, gtsam.Pose2(*edge.measurement()), noise))
+
+    def create_initial(self):
+        """Create initial estimates
+        """
+        initial_estimates = gtsam.Values()
+        for node in self.srg.nodes.values():
+            initial_estimates.insert(node.id_, gtsam.Pose2(*node.pose()))
+        self.initial = initial_estimates
+
+
+
 if __name__ == "__main__":
     import sys
     sys.path.append("../")
-    from process_g2o.utils import SingleRobotGraph
+    from process_g2o.utils import SingleRobotGraph2D
 
-    srg = SingleRobotGraph()
+    srg = SingleRobotGraph2D()
     srg.read_from("../datasets/manhattanOlson3500.g2o")
 
-    gtsam_graph = Graph(srg)
+    gtsam_graph = Graph2D(srg)
     gtsam_graph.optimize()
     print("======= Manhattan Graph Optimization =========")
     gtsam_graph.print_stats()
