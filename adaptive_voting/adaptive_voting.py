42e8f82e7bc45dc583d139b3edabe6f0e0f09807
sjxue <sjxue@mit.edu>
2019-12-05 16:06:34 -0500

    adaptive voting

---
+++ adaptive_voting/adaptive_voting.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+'''
+Implementation of algorithm 2 in TEASER paper
+'''
+
+def f(s,a,s_hat,c):
+    K=len(s)
+    objective = 0.
+    for i in range(K):
+        objective+=min((s_hat-s[i])**2/a[i]**2,c)
+    return objective
+
+
+def adaptive_voting(s,a,c):
+    '''
+    algorithm 2 of TEASER paper
+    Input: s_k, a_k, \bar{c}
+    Output: \hat{s} (Estimation of scale s)
+    '''
+    #Define boundaries and sort
+    K = len(s)
+    v=[]
+
+    for i in range(len(s)):
+        v.append(s[i]-a[i]*c)
+        v.append(s[i]+a[i]*c)
+    v=sorted(v)
+
+    #Compute middle points
+    m=[]
+    for i in range(2*K-1):
+        # m.append((v[2*i]+v[2*i+1])/2.)
+        m.append((v[i]+v[i+1])/2.)
+
+    #voting
+    S=[]
+    for i in range(2*K-1):
+        S.append([])
+
+        for k in range(K):
+
+            if m[i]<= s[k] + a[k]*c and m[i]>= s[k] - a[k]*c:
+                S[i].append(k)
+
+    #print S
+    #Enumerate consensus sets and return best
+
+    f_list=[]
+    for i in range(2*K-1):
+        ww=0.
+        ss = 0.
+        for k in S[i]:
+            ww+=1./(a[k]**2)
+            ss+=s[k]/(a[k]**2)
+        s_est = ss/ww
+        #print S[i],s_est,ww,ss
+        f_list.append((s_est,f(s,a,s_est,c)))
+
+    #print f_list
+    f_list  = sorted(f_list, key=lambda s:s[1])
+    return f_list[0][0]
+
+if __name__ == '__main__':
+    s = [1,2,2.8,3,3.2,4,5]
+    a = [1.1,1.1,1.3,1.3,1.1,1.1,1.1]
+    print(adaptive_voting(s,a,1))
