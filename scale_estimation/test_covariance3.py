e75da8c3140784983ebe9546b9b2cad3d046c6e9
sjxue <sjxue@mit.edu>
2019-12-08 23:30:30 -0500

    test joint marginal covariance

--- scale_estimation/test_covariance3.py
+++ scale_estimation/test_covariance3.py
@@ -11,28 +11,24 @@ import gtsam.utils.plot as gtsam_plot
 
 graph = gtsam.NonlinearFactorGraph()
 
+graph = gtsam.NonlinearFactorGraph()
+
 sb = np.zeros((3,3),dtype = np.float)
-np.fill_diagonal(sb, [0.2*0.2,0.2*0.2,0.1*0.1])
+
+np.fill_diagonal(sb, [0.2*0.2,0.5*0.5,0.1*0.1])
 print(sb)
 ODOMETRY_NOISE = gtsam.noiseModel_Gaussian.Covariance(sb)
-print(ODOMETRY_NOISE.R())
-print(ODOMETRY_NOISE.information())
 
-PRIOR_NOISE = gtsam.noiseModel_Diagonal.Sigmas(np.array([0.3, 0.3, 0.1],dtype = np.float))
-graph.add(gtsam.PriorFactorPose2(1, gtsam.Pose2(0.0, 0.0, 0.0), PRIOR_NOISE))
+PRIOR_NOISE = gtsam.noiseModel_Diagonal.Sigmas(np.array([1.6, 0.6, 0.1],dtype = np.float))
+graph.add(gtsam.PriorFactorPose2(1, gtsam.Pose2(0.0, 0.0, 1.0), PRIOR_NOISE))
 
-graph.add(gtsam.SIBetweenFactorPose2(1, 2, gtsam.Pose2(1.0, 0.0, 1.0), ODOMETRY_NOISE))
-graph.add(gtsam.SIBetweenFactorPose2(2, 3, gtsam.Pose2(-1.0, 1.0, 0.0), ODOMETRY_NOISE))
-graph.add(gtsam.SIBetweenFactorPose2(3, 1, gtsam.Pose2(0.0, -1.0, -0.5), ODOMETRY_NOISE))
-
-graph.add(gtsam.BetweenFactorPose2(1, 2, gtsam.Pose2(2.0, 0.0, 1.0), ODOMETRY_NOISE))
+graph.add(gtsam.BetweenFactorPose2(1, 2, gtsam.Pose2(2.0, 0.0, 0.0), ODOMETRY_NOISE))
+graph.add(gtsam.BetweenFactorPose2(2, 3, gtsam.Pose2(-2.0, 2.0, 0.0), ODOMETRY_NOISE))
 
 initial_estimate = gtsam.Values()
 initial_estimate.insert(1, gtsam.Pose2(0.5, 0.0, 0.2))
 initial_estimate.insert(2, gtsam.Pose2(2.3, 0.1, -0.2))
 initial_estimate.insert(3, gtsam.Pose2(4.1, 0.1, math.pi / 2))
-
-print("\nInitial Estimate:\n{}".format(initial_estimate))
 
 params = gtsam.LevenbergMarquardtParams()
 optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial_estimate, params)
@@ -46,7 +42,11 @@ key_vec.push_back(1)
 key_vec.push_back(2)
 key_vec.push_back(3)
 print('joint marginals')
-print(marginals.jointMarginalCovariance(key_vec).fullMatrix())
+jb = marginals.jointMarginalCovariance(key_vec).fullMatrix()
+jb[np.where(np.abs(jb)>1e-6)]=1
+jb[np.where(np.abs(jb)<=1e-6)]=0
+print(jb)
+
 
 
 marginals = gtsam.Marginals(graph, result)
