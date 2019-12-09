c2e89acfc6a590b693f5b649ad27806173d4d565
Jay Li <j_li@g.harvard.edu>
2019-12-08 22:53:48 -0500

    fix using perceptual aliasing only for 2D

--- process_g2o/convert_to_multi.py
+++ process_g2o/convert_to_multi.py
@@ -34,7 +34,8 @@ if __name__ == "__main__":
     print("========== Multi Robot g2o Graph Summary ================")
     multi_graph.print_summary()
 
-    multi_graph.add_perceptual_aliasing_lc(2, 8)
+    if not args.is_3D:
+      multi_graph.add_perceptual_aliasing_lc(2, 8)
     multi_graph.add_random_inter_lc(N=20)
     print("========== Noisy Multi Robot g2o Graph Summary ================")
     multi_graph.print_summary()
