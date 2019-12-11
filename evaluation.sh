fdc0f5ef2643ec40c2a594c3b910f7183ffe8ff4
sjxue <sjxue@mit.edu>
2019-12-11 08:26:38 -0500

    add SIFactor in optimize

--- evaluation.sh
+++ evaluation.sh
@@ -3,9 +3,9 @@ do
     echo
     echo "new_experiments"
     cd process_g2o
-    python3 convert_to_multi.py ../datasets/manhattanOlson3500.g2o --max_inter_lc 15 --random_inter_lc 9
+    python3 convert_to_multi.py ../datasets/manhattanOlson3500.g2o output1.g2o --max_inter_lc 15 --random_inter_lc 9
     cd ..
-    python3 multi_robot_optimization.py process_g2o/output.g2o
+    python3 multi_robot_optimization.py process_g2o/output1.g2o
 done
 
 for i in {1..20}
@@ -13,7 +13,7 @@ do
     echo
     echo "new_experiments"
     cd process_g2o
-    python3 convert_to_multi.py ../datasets/manhattanOlson3500.g2o --max_inter_lc 12 --random_inter_lc 12
+    python3 convert_to_multi.py ../datasets/manhattanOlson3500.g2o output1.g2o --max_inter_lc 12 --random_inter_lc 12 output_fpath
     cd ..
-    python3 multi_robot_optimization.py process_g2o/output.g2o
+    python3 multi_robot_optimization.py process_g2o/output1.g2o
 done
