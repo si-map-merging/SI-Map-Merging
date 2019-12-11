fix info scaling & add scaling b function

process_g2o/utils.py
@@ -8,7 +8,9 @@ from scipy.linalg import solve
 import numpy as np
 import quaternion
 import sophus as sp
-
+import sys
+sys.path.append("../")
+from scale_estimation.SIFactor import inv_Q
 
 def in_range(x, range_):
     return range_[0] <= x <= range_[1]
@@ -234,12 +236,30 @@ class Edge2D:
     def __mul__(self, scale):
         self.x *= scale
         self.y *= scale
-        self.info[0] *= scale * scale
-        self.info[1] *= scale * scale
-        self.info[2] *= scale
-        self.info[3] *= scale * scale
-        self.info[4] *= scale
+        self.info[0] /= scale * scale
+        self.info[1] /= scale * scale
+        self.info[2] /= scale
+        self.info[3] /= scale * scale
+        self.info[4] /= scale
 
+    def info_mat(self):
+        """
+        Return:
+            information matrix as 2D numpy array
+        """
+        N = 3
+        info_mat = np.zeros(shape=(N, N))
+        start = 0
+        for i in range(N):
+            info_mat[i, i:] = self.info[start: start + N-i]
+            start += N-i
+        info_mat = info_mat + info_mat.T - np.diag(info_mat.diagonal())
+
+        assert(np.allclose(info_mat, info_mat.T))
+        return info_mat
+
+    def cov(self):
+        return inv_Q(self.info_mat())
 
     def __str__(self):
         return "{} {} {} {} {} {}".format(self.i, self.j, self.x, self.y, self.theta,
@@ -285,7 +305,7 @@ class Edge3D:
         return info_mat
 
     def cov(self):
-        return np.linalg.inv(self.info_mat())
+        return inv_Q(self.info_mat())
 
     def measurement(self):
         R = Quaternion.to_R(self.q)
@@ -620,15 +640,25 @@ class MultiRobotGraph:
         if s_b is None:
             s_b = random.uniform(1, 10)
         for node in self.nodes[1].values():
+            node *= 1.0/s_b
+        for edge in self.odoms[1].values():
+            edge *= 1.0/s_b
+        for edge in self.lc[1].values():
+            edge *= 1.0/s_b
+        for edge in self.inter_lc.values():
+            if s_l is None:
+                s_l = random.uniform(1, 10)
+            edge *= 1.0/s_l
+
+    def scale_robot_b(self, s_b):
+        """Scale the translations of robot b by s_b
+        """
+        for node in self.nodes[1].values():
             node *= s_b
         for edge in self.odoms[1].values():
             edge *= s_b
         for edge in self.lc[1].values():
             edge *= s_b
-        for edge in self.inter_lc.values():
-            if s_l is None:
-                s_l = random.uniform(1, 10)
-            edge *= s_l
 
 
 class MultiRobotGraph2D(MultiRobotGraph):