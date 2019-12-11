13b9c1b4e65c6af174c373378363e274ddd866d0
sjxue <sjxue@mit.edu>
2019-12-11 03:18:50 -0500

    scale estimation

--- scale_estimation/SIFactor.py
+++ scale_estimation/SIFactor.py
@@ -20,6 +20,11 @@ def between(p1, p2):
     H2 = np.eye(size)
     return result,H1,H2
 
+def inv_Q(Q):
+    NOISE = gtsam.noiseModel_Gaussian.Covariance(Q)
+    Q_inv = NOISE.information()
+    return Q_inv
+
 def rotation_matrix_between_vectors(a,b):
     dim=3
     a = a.reshape(-1)
@@ -29,6 +34,10 @@ def rotation_matrix_between_vectors(a,b):
         a = np.hstack([a,[0]])
     if b.shape[0] == 2:
         b = np.hstack([b,[0]])
+    # print(type(np.linalg.norm(a)))
+    a = a/np.linalg.norm(a)
+    b = b/np.linalg.norm(b)
+
     v = np.cross(a,b)
     s = np.linalg.norm(v)
     c = a.dot(b)
@@ -37,21 +46,100 @@ def rotation_matrix_between_vectors(a,b):
         [v[2], 0, -v[0]],
         [-v[1], v[0], 0.],
         ])
-    # f = 
-    R = I + vx + vx @ vx *(1-c)/s**2
-    print(np.linalg.norm(v))
+    if s==0:
+        R = np.eye(3)
+    else:
+        R = np.eye(3) + vx + vx @ vx *(1-c)/s**2
+    # print(R)
+    if dim ==2:
+        R = R[0:2,0:2]
+    return R
+    #print(np.linalg.norm(v))
     # print (a,b)
     pass
 
-def get_scale(x,x_original,cov):
+def marginalization(M, index, inverse = inv_Q):
+    shape = M.shape[0]
+    #print('marginalization')
+    #print(M)
+    ind1 = index
+    ind2 = []
+    for i in range(shape):
+        if i not in index:ind2.append(i) 
+    # print(ind2)
+    A = M[ind1,:][:,ind1]
+    B = M[ind1,:][:,ind2]
+    BT = M[ind2,:][:,ind1]
+    C = M[ind2,:][:,ind2]
+    #print(A,B,BT,C)
+    # print(A)
+    result = A-B@inverse(C)@BT
+    #print(result)
+    return result
 
-    return s,var
+# print(marginalization(np.random.rand(3,3),[0,1],inverse = np.linalg.inv))
+
+def get_scale3(x_list,x_original_list,cov_list):
+    s_list = []
+    std_list = []
+    # result = []
+    for i in range(len(x_list)):
+        s,std_s = get_scale(x_list[i],x_original_list[i],cov_list[i])
+        # result.append([s,std_s])
+        s_list.append(s)
+        std_list.append(std_s)
+    s = np.array(s_list)
+    std = np.array(std_list)
+
+    w = np.sum(1./(std**2))
+    s_mean = np.sum(s/(std**2))/w
+    std = 1./np.sqrt(w)
+    return s_mean,std
+
+
+    # print(result)
+    # result = np.array(result)
+
+
+def get_scale(x,x_original,cov):
+    size = cov.shape[0]
+    dim = x.translation().vector().shape[0]
+
+    cov_translation = inv_Q(marginalization(inv_Q(cov), range(dim)))
+    # cov_trans = 
+
+    R_ij = x.rotation().matrix()
+
+    ex = np.zeros(dim)
+    ex[0] = 1
+    R_ki = rotation_matrix_between_vectors(x.translation().vector(), ex)
+
+    R_kj = R_ki @ R_ij
+
+    # R = np.block([
+    #     [R_kj, np.zeros((dim,size-dim))],
+    #     [np.zeros(size-dim,dim), np.eye(size-dim)]
+    #     ])
+
+    new_cov = R_kj @ cov_translation
+
+    var = inv_Q(marginalization(inv_Q(new_cov),[0]))
+    var = var.reshape(-1)[0]
+    #print(var)
+
+    l = np.linalg.norm(x.translation().vector())
+    l0 = np.linalg.norm(x_original.translation().vector())
+
+    std_l = np.sqrt(var)
+    #print('length')
+    #print(l0,l)
+    s = l0/l
+    std_s = s/l *std_l
+
+    return s,std_s
     pass
 
-def inv_Q(Q):
-    NOISE = gtsam.noiseModel_Gaussian.Covariance(Q)
-    Q_inv = NOISE.information()
-    return Q_inv
+
 
 def construct_SIFactor2(measured,cov):
     #measured = xi.between(xj)
@@ -168,7 +256,7 @@ def scale_covariance(cov, s):
     #print(translation)
     #return(measured,noise,H)
 
-# rotation_matrix_between_vectors(np.array([1,0]),np.array([0,1]))
+# print(rotation_matrix_between_vectors(np.array([1,0]),np.array([0.5,np.sqrt(3)/2])))
 
 if __name__ == '__main__':
     graph = gtsam.NonlinearFactorGraph()
@@ -225,14 +313,14 @@ if __name__ == '__main__':
     cov = marginals.jointMarginalCovariance(key_vec).fullMatrix()
     NOISE12 = cov_delta_xij(p1,p2,cov)
 
-    marginals = gtsam.Marginals(graph, result)
+    #marginals = gtsam.Marginals(graph, result)
     key_vec = gtsam.gtsam.KeyVector()
     key_vec.push_back(2)
     key_vec.push_back(3)
     cov = marginals.jointMarginalCovariance(key_vec).fullMatrix()
     NOISE23 = cov_delta_xij(p2,p3,cov)
 
-    marginals = gtsam.Marginals(graph, result)
+    #marginals = gtsam.Marginals(graph, result)
     key_vec = gtsam.gtsam.KeyVector()
     key_vec.push_back(1)
     key_vec.push_back(3)
@@ -246,9 +334,19 @@ if __name__ == '__main__':
     scale = 0.4
     print('scale: ',scale)
 
+    
+
     p1 = scale_pose(p1,scale)
     p2 = scale_pose(p2,scale)
     p3 = scale_pose(p3,scale)
+
+    # p1_original = p1
+    # p2_original = p2
+    # p3_original = p3
+
+    x_original_list = [p1.between(p2), p2.between(p3), p1.between(p3)]
+
+    # print(p1_original.between(),p1)
 
     NOISE12 = scale_covariance(NOISE12,scale)
     NOISE23 = scale_covariance(NOISE23,scale)
@@ -286,6 +384,46 @@ if __name__ == '__main__':
     result = optimizer.optimize()
 
     marginals = gtsam.Marginals(graph, result)
+
+
+
+    p1 = result.atPose2(1)
+    p2 = result.atPose2(2)
+    p3 = result.atPose2(3)
+
+
+    marginals = gtsam.Marginals(graph, result)
+
+    key_vec = gtsam.gtsam.KeyVector()
+    key_vec.push_back(1)
+    key_vec.push_back(2)
+    cov = marginals.jointMarginalCovariance(key_vec).fullMatrix()
+    NOISE12 = cov_delta_xij(p1,p2,cov)
+
+    key_vec = gtsam.gtsam.KeyVector()
+    key_vec.push_back(2)
+    key_vec.push_back(3)
+    cov = marginals.jointMarginalCovariance(key_vec).fullMatrix()
+    NOISE23 = cov_delta_xij(p2,p3,cov)
+
+    key_vec = gtsam.gtsam.KeyVector()
+    key_vec.push_back(1)
+    key_vec.push_back(3)
+    cov = marginals.jointMarginalCovariance(key_vec).fullMatrix()
+    NOISE13 = cov_delta_xij(p1,p3,cov)
+
+    cov_list = [NOISE12, NOISE23, NOISE13]
+
+    x12 = p1.between(p2)
+
+    x_list = [p1.between(p2), p2.between(p3), p1.between(p3)]
+    print('get scale')
+    print(get_scale(x_list[0],x_original_list[0],cov_list[0]))
+
+    print('get scale 3')
+    print(get_scale3(x_list,x_original_list,cov_list))
+
+
     # for i in range(1, 4):
     #     print("X{} covariance:\n{}\n".format(i, marginals.marginalCovariance(i)))
 
